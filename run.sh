f0d8130aa17504ba2f19c4d9fe4d6aa8e54dfa8d
Inba <inba2002.p@gmail.com>
2025-04-14 21:01:23 +0530

    update process interval

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 python vehicle_detection.py --source ./data/vid-1.MOV --device cpu \
- --roi-x1 25 --roi-y1 75 --roi-x2 100 --roi-y2 100 --process-interval 3.0 --exit-zone-percent 20
+ --roi-x1 25 --roi-y1 75 --roi-x2 100 --roi-y2 100 --process-interval 3.0 -process-interval 3.0
